3772841f335e9fd178336e564f11b78bd0ced6ec
selfstudymaster <webselfmaster@gmail.com>
2021-03-11 10:14:46 +0900

    ディレクトリ名変更

---
+++ calc/Calc.java
@@ -0,0 +1,19 @@
+package calc;
+
+// chap06
+// list06-01
+public class Calc {
+	public static void calc(String[] args) {
+
+		System.out.println("テスト");
+	}
+}
+
+
+// 雛形
+//public class Main {
+//	public static void main(String[] args) {
+//
+//		System.out.println("テスト");
+//	}
+//}
