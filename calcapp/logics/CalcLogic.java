b0de59c70efb57ff7dde80422a63cb2951c98784
selfstudymaster <webselfmaster@gmail.com>
2021-03-14 18:17:03 +0900

    写経と練習(終了後削除済)

--- calcapp/logics/CalcLogic.java
+++ calcapp/logics/CalcLogic.java
@@ -1,9 +1,11 @@
 package calcapp.logics;
 
 public class CalcLogic {
+	// tasuメソッド
 	public static int tasu(int a, int b) {
 		return (a + b);
 	}
+	// hikuメソッド
 	public static int hiku(int a, int b) {
 		return (a - b);
 	}
--- calcapp/main/Calc.java
+++ calcapp/main/Calc.java
@@ -11,11 +11,11 @@ public class Calc {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 2;
-		int total = CalcLogic.tasu(a, b);
-		int delta = calcapp.logics.CalcLogic.hiku(a, b);
+		int total = CalcLogic.tasu(a, b); // CalcLogicクラスのtasuメソッドをtotalに代入
+		int delta = calcapp.logics.CalcLogic.hiku(a, b); // CalcLogicクラスのhikuメソッドをdeltaに代入
+//		System.out.println("足すと" + total + "、引くと" + delta);
 		System.out.println("足すと" + total + "、引くと" + delta);
 	}
-
 }
 
 
